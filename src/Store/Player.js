a7f4ddadd50a79cd327e6e1ff7f631f4649dd015
akhilmhdh <akhilmhdh@gmail.com>
2020-07-13 20:07:52 +0530

    added select

--- src/Store/Player.js
+++ src/Store/Player.js
@@ -1,13 +1,14 @@
 import { writable } from 'svelte/store';
 
 const CreatePlayerStore = () =>{
-    const { subscribe, set, update } = writable({state:false,BPM:180});
+    const { subscribe, set, update } = writable({state:false,BPM:180,direction:'To Right ->'});
     return {
         subscribe,
-        play:() => update(config => ({...config,state:true})),
-        stop:() => update(config => ({...config,state:false})),
-        toggle:() => update(config => ({...config,state:!config.state})),
-        changeBPM:(val) => update(config => ({...config,BPM:val})),
+        play: () => update(config => ({...config,state:true})),
+        stop: () => update(config => ({...config,state:false})),
+        toggle: () => update(config => ({...config,state:!config.state})),
+        changeBPM: (val) => update(config => ({...config,BPM:val})),
+        changeDirection: (direction) => update(config => ({...config,direction})),
     }
 }
 
